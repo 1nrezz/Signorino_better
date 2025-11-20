fcbbc2de4f7195aacd264766045a6385b2f8a120
1nrez <formyyworkk@gmail.com>
2025-11-20 12:33:29 +0100

    test1

--- main/main.py
+++ main/main.py
@@ -1,1 +1,23 @@
-print("dolbaeb")
+import discord
+from discord.ext import commands
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv("TOKEN")
+guild_ids=[1440996306251153440]
+PREFIX = "/"
+
+intents = discord.Intents().all()
+bot = commands.Bot(command_prefix=PREFIX, intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f"Logged in as {bot.user}")
+
+@bot.slash_command(name="ping", description="Проверка бота", guild_ids=guild_ids)
+async def ping(ctx):
+    await ctx.respond("ping!")
+    await ctx.send("Pong!")
+
+bot.run(TOKEN)
